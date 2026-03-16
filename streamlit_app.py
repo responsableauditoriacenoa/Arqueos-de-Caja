5ce38312e447d0da835fa95b941ade43ab37a0d0
responsableauditoriacenoa <responsable.auditoriacenoa@gmail.com>
2026-03-16 10:20:26 -0300

    fix: entrypoint streamlit estable con acceso directo a SPA

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title='Arqueos de Caja', layout='wide')
 
-st.markdown("""
-<meta http-equiv="refresh" content="0; url=/static/index.html#/" />
-<div style="padding: 1rem; font-family: sans-serif;">
-    Redirigiendo a la aplicación...
-    <a href="/static/index.html#/" target="_self">Abrir ahora</a>
-</div>
-""", unsafe_allow_html=True)
+st.title('Arqueos de Caja')
+st.caption('Acceso a la aplicación web')
+
+st.link_button('Abrir aplicación', '/static/index.html#/', use_container_width=True)
+
+st.info('Si el botón no abre automáticamente, copiá esta URL en el navegador: /static/index.html#/')
