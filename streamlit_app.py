08467a2e7138c4118404901267f48c2139e81323
responsableauditoriacenoa <responsable.auditoriacenoa@gmail.com>
2026-03-16 10:57:32 -0300

    fix: usar /app/static para cargar SPA en streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.set_page_config(page_title='Arqueos de Caja', layout='wide')
 st.title('Arqueos de Caja')
 st.caption('Acceso a la aplicación web')
 
-st.link_button('Abrir aplicación', '/static/index.html#/', use_container_width=True)
+st.link_button('Abrir aplicación', '/app/static/index.html#/', use_container_width=True)
 
-st.info('Si el botón no abre automáticamente, copiá esta URL en el navegador: /static/index.html#/')
+st.info('Si el botón no abre automáticamente, copiá esta URL en el navegador: /app/static/index.html#/')
